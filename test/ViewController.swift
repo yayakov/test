6aacb8d9d5ce23aa71c3d6ec609fde428ef98e35
優しく <yasashiku@yasashiku.local>
2018-03-17 15:21:04 +0900

    final

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("shit...")
+        print("shit...new")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
